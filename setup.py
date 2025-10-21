9e2e2cae1dea90c4669494ecb43ede8b49b31f3d
ayushmehraa <mehrayush762@gmail.com>
2025-10-21 20:21:12 +0530

    setting up

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """This function will return the list of requirements"""
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip()]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+            
+    return requirements
+
+setup(
+    name="my_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires= get_requirements("requirements.txt")
+)
